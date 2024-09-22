3c54a8d3212eb9f28a4979f82b24f726a59e933f
767961652@qq.com <767961652@qq.com>
2024-09-22 16:06:47 +0800

    submit to another computer

--- vue-project/src/components/testPage/testPage.vue
+++ vue-project/src/components/testPage/testPage.vue
@@ -6,17 +6,23 @@
             <input v-model="question" :disabled="loading" />
         </p>
         <p>{{ answer }}</p>
+        <div>
+            <div class="ns">1</div>
+            <div class="ns">2</div>
+            <div class="ns">3</div>
+            <div class="ns">4</div>
+        </div>
     </div>
 </template>
 
 <script setup>
     import { ref, watch } from 'vue'
 
+    //async函数
     const question = ref('')
     const answer = ref('Questions usually contain a question mark. ;-)')
     const loading = ref(false)
 
-    // 可以直接侦听一个 ref
     watch(question, async (newQuestion, oldQuestion) => {
     if (newQuestion.includes('?')) {
         loading.value = true
@@ -31,6 +37,22 @@
         }
     }
     })
+
+
+    //试一试set
+    // const set = new Set(document.querySelectorAll('div'));
+    // console.log(set);
+    // console.log(set.size);
+    // console.log(typeof set);
+    const arr1 = [2,5,6,8,5,3,1,2];
+    const arr2 = [...new Set(arr1)];
+    console.log(arr1);
+    
+    console.log(arr2);
+    
+    
+    
+
 </script>
 
 <style>
